8790fc5be0331f738249125638c980c12c775bf9
Viktor Nagels <71647271+viktor-nagels@users.noreply.github.com>
2022-01-26 10:46:26 +0100

    progress

--- raspberry/project.py
+++ raspberry/project.py
@@ -1,6 +1,10 @@
 #ultra sonic sensor
 import RPi.GPIO as GPIO
 import time
+import sys 
+from pubnub.pnconfiguration import PNConfiguration 
+from pubnub.pubnub import PubNub 
+from pubnub.callbacks import SubscribeCallback 
 
 ultrasonic1 = 20
 ultrasonic2 = 21
@@ -28,6 +32,25 @@ def ultrasonic():
     distance = timepassed * 17000
     return distance
 
+pnconfig = PNConfiguration()
+pnconfig.subscribe_key = 'sub-c-91c29a42-7e21-11ec-8e41-c2c95df3c49a'
+pnconfig.publish_key = 'pub-c-d79f4642-99a1-44fa-9ece-708cde163413'
+pnconfig.uuid = 'p9Mn66G4D5cOmBlSJSFCmSV8uQn2'
+pubnub = PubNub(pnconfig)
+
+
+
+def my_publish_callback(envelope, status):
+
+    # Check whether request successfully completed or not
+
+    if not status.is_error():
+
+        pass  # Message successfully published to specified channel.
+
+    else:
+
+        pass
 try:
     while True:
         distance = ultrasonic()
@@ -35,9 +58,12 @@ try:
         if distance >= 30:
             print(distance)
             GPIO.output(delay, 1)
-        else:
+        if distance <= 25:
             GPIO.output(delay, 0)
+            print(distance)
+        
+        pubnub.publish().channel('p9Mn66G4D5cOmBlSJSFCmSV8uQn2').message(str(distance)).pn_async(my_publish_callback)
  
-        time.sleep(1)
+        time.sleep(5)
 finally:
     GPIO.cleanup()

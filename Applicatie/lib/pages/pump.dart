2f4c69d37387020a85fc279129eb277ecd59d88d
Viktor Nagels <71647271+viktor-nagels@users.noreply.github.com>
2022-01-27 13:52:31 +0100

    pump page

--- Applicatie/lib/pages/pump.dart
+++ Applicatie/lib/pages/pump.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pubnub/pubnub.dart';
 
 class PumpPage extends StatefulWidget {
   const PumpPage({Key? key}) : super(key: key);
@@ -8,11 +9,30 @@ class PumpPage extends StatefulWidget {
 }
 
 class _PumpPageState extends State<PumpPage> {
-  @override
+  var pubnub = PubNub(
+      defaultKeyset: Keyset(
+          subscribeKey: 'sub-c-91c29a42-7e21-11ec-8e41-c2c95df3c49a',
+          publishKey: 'pub-c-d79f4642-99a1-44fa-9ece-708cde163413',
+          uuid: const UUID('p9Mn66G4D5cOmBlSJSFCmSV8uQn2')));
+
   Widget build(BuildContext context) {
-    return const Text(
-      'Pomp',
-      style: TextStyle(color: Colors.white),
+    final ButtonStyle style = ElevatedButton.styleFrom(
+        textStyle: const TextStyle(fontSize: 20), primary: Colors.blue);
+
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          const SizedBox(height: 30),
+          ElevatedButton(
+            style: style,
+            onPressed: () {
+              pubnub.publish('pump-p9Mn66G4D5cOmBlSJSFCmSV8uQn2', '1');
+            },
+            child: const Text('Pump water'),
+          ),
+        ],
+      ),
     );
   }
 }
